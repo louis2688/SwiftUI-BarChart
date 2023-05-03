ef1e11c9ad3df65fbb21b69da1cccb948b9cbd51
louis2688 <louismadrigal26@gmail.com>
2023-05-04 01:29:47 +0800

    Initial Commit

---
+++ SwiftUI-BarChart/SwiftUI_BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarChartApp.swift
+//  SwiftUI-BarChart
+//
+//  Created by dev on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
